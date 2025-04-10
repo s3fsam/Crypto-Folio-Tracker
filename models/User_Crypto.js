b77a856bf3ea74c222df45b52e813bc506f9998d
s3fsam <michaelpartouche@geekonline.fr>
2025-04-10 19:42:17 +0200

    Update User_Crypto.js

--- models/User_Crypto.js
+++ models/User_Crypto.js
@@ -5,7 +5,9 @@ const userCryptoSchema = new mongoose.Schema({
   address: { type: String, required: true, unique: true },
   balance: { type: Number, default: 0 },
   delimiterStart: { type: String, required: false },
-  delimiterEnd: { type: String, required: false }
+  delimiterEnd: { type: String, required: false },
+  cssSelector: { type: String, required: false } // <-- ✅ AJOUT ICI
 }, { timestamps: true });
 
 module.exports = mongoose.model('UserCrypto', userCryptoSchema);
+
